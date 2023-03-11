bb3bd98e9725b5d2ee55fb4d0d0631337d4b6b17
Gwei111 <2663215956@qq.com>
2023-03-11 14:47:50 +0800

    打包优化

--- exam/vite.config.ts
+++ exam/vite.config.ts
@@ -1,22 +1,11 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteImagemin   from "vite-plugin-imagemin";
-import viteCompression from 'vite-plugin-compression'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),viteImagemin(),viteCompression()],
+  plugins: [vue()
+  ],
   base: './',
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        //生产环境时移除console
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    sourcemap: false,
-  },
+
 
 })
